d28bb45a32236b384963b462c2a31453a1c507fe
Dorin Paslaru <dpaslaru@cloudbasesolutions.com>
2014-09-17 19:06:47 +0300

    new tests, bug fixes
    VSS, timesync, smp

--- tempest/lis/Networking/test_network.py
+++ tempest/lis/Networking/test_network.py
@@ -1,5 +1,4 @@
-# Copyright 2012 OpenStack Foundation
-# All Rights Reserved.
+# Copyright 2014 Cloudbase Solutions Srl
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
