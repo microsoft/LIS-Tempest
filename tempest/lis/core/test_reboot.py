f67195afef8159f6b3e0bc47e74f66f1400bbb76
Zsolt Dudás <zdudas@cloudbasesolutions.com>
2016-04-07 17:30:24 +0300

    Imported waiters

--- tempest/lis/core/test_reboot.py
+++ tempest/lis/core/test_reboot.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from tempest.common import waiters
 from tempest import config
 from tempest import exceptions
 from tempest import test
--- tempest/lis/core/test_storage.py
+++ tempest/lis/core/test_storage.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from tempest.common import waiters
 from tempest import config
 from oslo_log import log as logging
 from tempest.lis import manager
